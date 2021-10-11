147e590f1f574d17010d9181bff944df143f51b0
zeenat08 <zeenatkhanbfs@gmail.com>
2021-10-11 22:09:01 +0530

    ZEENAT: [Added] Custom Exception

---
+++ Day20_Regex_Test_Assignments/Custom_Exception/UserRegistrationCustomException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day20_Regex_Test_Assignments.Custom_Exception
+{
+    public class UserRegistrationCustomException : Exception
+    {
+        public enum ExceptionsType
+        {
+            INVALID_MESSAGE
+        }
+        public ExceptionsType type;
+        public UserRegistrationCustomException(ExceptionsType Type, string message) : base(message)
+        {
+            this.type = Type;
+
+        }
+
+    }
+}
+
+    
+
