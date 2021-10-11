6da11e20812f43a352ac4a80d5e95c1a6a939e5f
zeenat08 <zeenatkhanbfs@gmail.com>
2021-10-11 21:46:14 +0530

    ZEENAT: [Added] Regex pattern for Last name of the user

---
+++ Day20_Regex_Test_Assignments/UC2_LastName/LastName_Pattern.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Day20_Regex_Test_Assignments.Custom_Exception;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Day20_Regex_Test_Assignments.UC2_LastName
+{
+        public class LastName_Pattern
+        {
+            public static string Regex_Lastname = "^[A-Z]{1}[a-z]{2,}$";
+
+            public bool ValidateLastName(string lastname) //method to check lastname
+            {
+                if (lastname == null)
+                {
+                    throw new Custom_Exception.UserRegistrationCustomException(UserRegistrationCustomException.ExceptionsType.INVALID_MESSAGE, "Invalid LastName");
+                }
+                return Regex.IsMatch(lastname, Regex_Lastname);
+            }
+        }
+}
