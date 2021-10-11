10fe1f158a684c3abf4148dc62acf2dc5b5f643a
zeenat08 <zeenatkhanbfs@gmail.com>
2021-10-11 22:04:20 +0530

    ZEENAT: [Added] Regex pattern for Password of the user

---
+++ Day20_Regex_Test_Assignments/UC5_6_7_8_Password/Password_Pattern.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using Day20_Regex_Test_Assignments.Custom_Exception;
+
+namespace Day20_Regex_Test_Assignments.UC5_6_7_8_Password
+{
+    public class Password_Pattern
+        {
+            //public static string Regex_PSWD = "^([$#.%+@-~!]{1}([a-zA-Z0-9]*[A-Z]{1,}[0-9]{1,})){8,}$";
+            public static string Regex_PSWD = "(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+=-])[a-zA-Z0-9!@#$%^&*()_+=-]{8,}$";
+            public bool ValidatePassword(string password)
+            {
+                if (password == null)
+                {
+                    throw new UserRegistrationCustomException(UserRegistrationCustomException.ExceptionsType.INVALID_MESSAGE, "Invalid Password");
+                }
+                return Regex.IsMatch(password, Regex_PSWD);
+            }
+    }
+}
+
