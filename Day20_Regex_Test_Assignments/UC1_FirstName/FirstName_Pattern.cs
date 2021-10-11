e2005d8824ae18a01451e662fc2df2acb0d455d8
zeenat08 <zeenatkhanbfs@gmail.com>
2021-10-11 21:44:16 +0530

    ZEENAT: [Added] Regex pattern for first name of the user

---
+++ Day20_Regex_Test_Assignments/UC1_FirstName/FirstName_Pattern.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Day20_Regex_Test_Assignments.Custom_Exception;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+
+namespace Day20_Regex_Test_Assignments.UC1_FirstName
+
+{
+    public class FirstName_Pattern
+    
+    {
+            public static string Regex_Firstname = "^[A-Z]{1}[a-z]{2,}$";
+            public bool ValidateFirstName(string firstName)//method to check firstname
+            {
+                if (firstName == null)
+                {
+                    throw new UserRegistrationCustomException(UserRegistrationCustomException.ExceptionsType.INVALID_MESSAGE, "Invalid FirstName");
+                }
+                return Regex.IsMatch(firstName, Regex_Firstname);
+            }
+    }
+}
